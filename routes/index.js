a5b7b1119f327d2bc0eb57b8afd0e47340d67467
Gilbert <gilbert.torchon@esih.edu>
2018-04-25 19:45:10 -0400

    design added. /whoami route added.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var net=require('net')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { hostname:req.headers.host });
 });
+
+router.get('/whoami', function(req, res, next) {
+
+  var languageRegex=/^(\S)*,(\S)*$/;
+  var softwareRegex=//;
+
+  res.json({ipadress: req.header('x-forwarded-for') || req.connection.remoteAddress,language:req.headers['accept-language'],sofware:req.headers['user-agent']})
+});
+
+
 
 module.exports = router;
